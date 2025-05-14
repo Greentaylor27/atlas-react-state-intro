2e387ee16fcd3c13cbf2682c13bf5883bb3a5a57
Greentaylor27 <greentaylor18@gmail.com>
2025-05-14 14:08:01 -0500

    Filtering is complete and seems to be working as the instructions have asked.

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 export default function SchoolCatalog() {
   const [courses, setCourses] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     fetch('/api/courses.json')
@@ -13,7 +14,7 @@ export default function SchoolCatalog() {
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
-      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search" value={filter} onChange={(e) => setFilter(e.target.value)} />
       <table>
         <thead>
           <tr>
@@ -26,7 +27,12 @@ export default function SchoolCatalog() {
           </tr>
         </thead>
         <tbody>
-          {courses.map((courses, index) => (
+          {courses
+            .filter(courses => 
+              courses.courseName.toLowerCase().includes(filter.toLowerCase()) ||
+              courses.courseNumber.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((courses, index) => (
             <tr key={index}>
               <td>{courses.trimester}</td>
               <td>{courses.courseNumber}</td>
